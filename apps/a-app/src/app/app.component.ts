910d21b6c9e1212d0a8b75a68ad33782db5f5b86
cgraha217 <charles_graham@comcast.com>
2022-11-18 10:07:00 -0700

    Initial commit

---
+++ apps/a-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'package-deployment-workspace-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'a-app';
+}
